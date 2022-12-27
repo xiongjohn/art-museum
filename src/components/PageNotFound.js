f3568a69472bede68c142e2ca56b8cb570b81057
Manna Kong <m.kong.57@gmail.com>
2022-12-26 21:07:42 -0600

    added link to gallery for page not found
    Signed-off-by: Manna Kong <m.kong.57@gmail.com>

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,10 +1,12 @@
+import { NavLink } from "react-router-dom"
+
 function PageNotFound() {
     return (
         <div>
             <h2>Page Not Found</h2>
             <h2>Page Not Found</h2>
             <h2>Page Not Found</h2>
-
+            <p>Try another page or head back to the <NavLink to="/" end>Gallery</NavLink></p>
         </div>
     )
 }
