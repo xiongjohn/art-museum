f19f8b386192550fa953006d3c7b24207947e817
saraikong <115051994+xiongjohn@users.noreply.github.com>
2023-01-26 22:57:45 -0600

    Added styling to components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,8 @@
 function Home() {
   return (
     <div>
-      <h2>Harvard Art Museum</h2>
-      <p>
+      <h2 className="text-center">Harvard Art Museum</h2>
+      <p className="text-center">
         Look, but Don't Touch. Please select a Gallery in the navigation bar.
       </p>
     </div>
